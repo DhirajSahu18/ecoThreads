8f0a218e47e2adb061253bfb8caefac434edc396
Dhiraj Sahu <dhiraj.sahu@atriina.com>
2025-04-14 22:03:59 +0530

    added client full

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -62,7 +62,7 @@ export const login = async (req, res) => {
       { expiresIn: '7d' }
     );
 
-    res.status(200).json({
+    res.status(200).json( {
       message: 'Login successful',
       user: {
         id: user._id,
